07b6ded19ac17fc344b2a2aab9b7da459fa0b7f8
Reinaldo Aguilar <reinaldo.aguilar@communitybrands.com>
2020-08-22 19:08:11 -0400

    Adding the isValidPosition and RemovePiece

---
+++ Chess/Chess/Pieces/BoardException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.Pieces
+{
+    public class BoardException : Exception
+    {
+        public BoardException(string message) : base(message)
+        {
+        }
+    }
+}
